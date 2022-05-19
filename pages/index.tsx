f069c8fe3b5655762d6d4ffa7adccc8e20fb002e
laura <laura.duboeuf68@gmail.com>
2022-05-19 11:15:08 +0200

    add Button create ticket and pop Modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Modal from "../components/Modal";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Console log project</h1>
+      <Modal />
     </div>
   );
 };
