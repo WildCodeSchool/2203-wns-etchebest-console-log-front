1da1e9412b0a6094bdbdda691a13f3559c222243
Emeline <leduc.emeline44@gmail.com>
2022-05-20 09:39:58 +0200

    refacto apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
-import { ApolloProvider } from "@apollo/client";
-import { useApollo } from "../lib/apollo";
 import "../styles/globals.css";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps);
   return (
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
   );
