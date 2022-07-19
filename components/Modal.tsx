c0e1d95169fe1f96659a82ca84736b1e687cab34
laura <laura.duboeuf68@gmail.com>
2022-07-19 16:45:52 +0200

    delete consolelog

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -7,10 +7,9 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 import TextField from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
-import Stack from "@mui/material/Stack";
 import CREATE_ONE_TICKET from "../lib/queries/createOneTicket";
 import { useMutation } from "@apollo/client";
-import { useState } from "react";
+
 
 const style = {
   position: "absolute" as "absolute",
@@ -36,18 +35,14 @@ const BasicModal = () => {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    console.log("avant");
     createOneTicket({
       variables: { data: { title: title, description: description } },
     });
-    console.log("après");
     setTitle("");
     setDescription("");
     handleClose();
-    /* setTrigger(Date.now()); */
-    /*  window.location.reload(); */
+
   };
-  console.log("handlesubmitTEST", { handleSubmit });
   if (loading) return "Submitting...";
   if (error) return `Submission error! ${error.message}`;
 
@@ -122,3 +117,133 @@ const BasicModal = () => {
 
 export default BasicModal;
 
+// import * as React from "react";
+// import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
+// import Modal from "@mui/material/Modal";
+// import AddCircleIcon from "@mui/icons-material/AddCircle";
+// import TextField from "@mui/material/TextField";
+// import IconButton from "@mui/material/IconButton";
+// import Tooltip from "@mui/material/Tooltip";
+// import Stack from "@mui/material/Stack";
+// import CREATE_TICKET from "../lib/queries/createTicket";
+// import { useMutation } from "@apollo/client";
+
+// const style = {
+//   position: "absolute" as "absolute",
+//   top: "50%",
+//   left: "50%",
+//   transform: "translate(-50%, -50%)",
+//   width: 400,
+//   bgcolor: "background.paper",
+//   border: "2px solid #000",
+//   boxShadow: 24,
+//   p: 4,
+// };
+
+// const BasicModal: React.FC = () => {
+//   const [open, setOpen] = React.useState(false); // use state qui permet de modifier les booleans lors des ouverture et fermeture
+//   /*   === fonctions qui changent le useState open de true à false === */
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
+
+//   const [createTicket, { data, loading, error }] = useMutation(CREATE_TICKET);
+//   let input: any;
+//   if (loading) return "Submitting...";
+//   if (error) return `Submission error! ${error.message}`;
+
+//   return (
+//     <Box maxWidth="xl" sx={{ textAlign: "end" }}>
+//       {/*   === debut du bouton AddCircle ===  */}
+//       <Tooltip title="Create a new ticket">
+//         <IconButton sx={{ marginTop: "1em", marginRight: "0.4em" }}>
+//           <AddCircleIcon
+//             sx={{
+//               color: "#1AB983",
+//               width: "1.2em",
+//               height: "1.2em",
+//             }}
+//             onClick={handleOpen}
+//           />
+//         </IconButton>
+//       </Tooltip>
+//       {/*   === fin du bouton AddCircle ===  */}
+
+//       {/*   === debut  de la Modal ===  */}
+//       <Modal
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="modal-modal-title"
+//       >
+//         <Box>
+//           <Box sx={style}>
+//             <Typography id="modal-modal-title" variant="h6" component="h2">
+//               Create a new ticket
+//             </Typography>
+//             <Typography id="modal-modal-description" sx={{ mt: 2, mb: 3 }}>
+//               <Box
+//                 component="form"
+//                 sx={{
+//                   "& .MuiTextField-root": { width: "100%" },
+//                 }}
+//                 noValidate
+//                 autoComplete="off"
+//               >
+//                 <TextField
+//                   id="outlined-textarea"
+//                   label="The title of the new ticket"
+//                   placeholder="Title"
+//                   multiline
+//                 />
+//               </Box>
+//             </Typography>
+//             <Typography id="modal-modal-description" sx={{ pt: 2, pb: 3 }}>
+//               <Box
+//                 component="form"
+//                 sx={{
+//                   "& .MuiTextField-root": { width: "100%" },
+//                 }}
+//                 noValidate
+//                 autoComplete="off"
+//                 onSubmit={async (e: any) => {
+//                   e.preventDefault();
+//                   await createTicket({
+//                     variables: { type: input.description },
+//                   });
+//                   input.value = "description";
+//                 }}
+//                 ref={(node) => {
+//                   input = node;
+//                 }}
+//               >
+//                 <TextField
+//                   id="outlined-textarea"
+//                   label="Ticket description"
+//                   placeholder="Description"
+//                   multiline
+//                   rows={4}
+//                 />
+
+//                 <Stack direction="row" spacing={2}>
+//                   <Button
+//                     type="submit"
+//                     variant="contained"
+//                     color="success"
+//                     sx={{ backgroundColor: "#1AB983 !important", mt: 3 }}
+//                   >
+//                     Add
+//                   </Button>
+//                 </Stack>
+//               </Box>
+//             </Typography>
+//             <Stack direction="row" spacing={2}>
+//             </Stack>
+//           </Box>
+//         </Box>
+//       </Modal>
+//     </Box>
+//   );
+// };
+
+// export default BasicModal;
